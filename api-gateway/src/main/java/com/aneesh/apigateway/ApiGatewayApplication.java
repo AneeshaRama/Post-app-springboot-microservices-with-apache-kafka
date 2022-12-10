5f1b916fcb7faecd6fa9eb64e1854ec91f08a89f
Aneesha Rama <ARama@Opteamix.com>
2022-12-10 16:00:03 +0530

    Configured Zuul api gateway

--- api-gateway/src/main/java/com/aneesh/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/aneesh/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,79 @@
 package com.aneesh.apigateway;
 
+import com.aneesh.apigateway.zuulFilter.ErrorFilter;
+import com.aneesh.apigateway.zuulFilter.PostFilter;
+import com.aneesh.apigateway.zuulFilter.PreFilter;
+import com.aneesh.apigateway.zuulFilter.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.cloud.netflix.zuul.web.ZuulController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collection;
+import java.util.List;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public ServerCodecConfigurer serverCodecConfigurer() {
+		return ServerCodecConfigurer.create();
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+
+	@Bean
+	public ErrorFilter zuulErrorFilter()
+	{
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public PostFilter zuulPostFilter()
+	{
+		return new PostFilter();
+	}
+
+	@Bean
+	public PreFilter zuulPreFilter()
+	{
+		return new PreFilter();
+	}
+
+	@Bean
+	public RouteFilter zuulRouteFilter()
+	{
+		return new RouteFilter();
 	}
 
 }
