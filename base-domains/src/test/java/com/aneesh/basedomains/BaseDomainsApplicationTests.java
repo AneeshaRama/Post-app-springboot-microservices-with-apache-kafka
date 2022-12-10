a592907bd9cb621283ccda1423c358d7a6171643
Aneesha Rama <ARama@Opteamix.com>
2022-12-10 12:32:50 +0530

    First commit

---
+++ base-domains/src/test/java/com/aneesh/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aneesh.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
