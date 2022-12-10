f9754e9df29f99f77e205f20593ef6c0c28c4402
Aneesha Rama <ARama@Opteamix.com>
2022-12-10 12:48:01 +0530

    Created base domains

---
+++ base-domains/src/main/java/com/aneesh/basedomains/entities/Comment.java
@@ -0,0 +1,18 @@
+package com.aneesh.basedomains.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Comment {
+
+    private long commentId;
+
+    private String commentContent;
+
+}
